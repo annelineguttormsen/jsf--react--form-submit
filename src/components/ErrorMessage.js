230c98b95574082569162835be2f4f6dc380ccce
Anneline <anneline.guttormsen@hotmail.no>
2020-03-13 15:20:41 +0100

    className not class

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function ErrorMessage(props) {
     return (
-        <p class="error__message">{props.message}</p>
+        <p className="error__message">{props.message}</p>
     );
 }
